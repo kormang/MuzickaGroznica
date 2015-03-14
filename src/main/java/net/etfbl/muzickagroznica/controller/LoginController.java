a8bc7007f99e361f4990b41f22aac0d0c9cada16
Marko Ivanovic <kormang@hotmail.rs>
2015-03-14 13:40:54 +0100

    Spring security podesen, ali nije testiran. Ostali su problemi sa TX+CP.

---
+++ src/main/java/net/etfbl/muzickagroznica/controller/LoginController.java
@@ -0,0 +1,30 @@
+package net.etfbl.muzickagroznica.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class LoginController {
+
+	public LoginController() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public ModelAndView login(Model model,
+			@RequestParam(value = "failure", required = false)String failure){
+		
+		ModelAndView ret = new ModelAndView("login");
+		
+		if("1".equals(failure)){
+			ret.addObject("failure", true);
+		}
+		
+		return ret;
+	}
+
+}
